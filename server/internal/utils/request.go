792ac03ed649e228e3ac5fe9ec3755b4fadae1da
XIA <koryxia@icloud.com>
2025-04-10 15:20:20 +0200

    ✨ feat(internal/utils): add Bind function for request handling

---
+++ server/internal/utils/request.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"server/internal/constants/bizErr"
+	"server/internal/models/responses"
+)
+
+func Bind[T any](c *gin.Context, request T) error {
+	if err := c.ShouldBind(request); err != nil {
+		var res responses.BaseResponse
+		res.ErrorResponse(c, http.StatusBadRequest, bizErr.ParseParamsErr)
+		return err
+	}
+	return nil
+}
